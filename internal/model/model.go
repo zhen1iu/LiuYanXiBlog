5a576fc0a2e677b84b5f7dd9bff1fe070c7d3302
zhen1iu <937631064@qq.com>
2024-08-12 18:15:22 +0800

    重构

---
+++ internal/model/model.go
@@ -0,0 +1,22 @@
+package model
+
+
+type Model struct {
+	ID uint32 `gorm:"primary_key"` `json:id` 
+	CreatedBy string `json:"created_by"`
+	ModifiedBy string `json:"modified_by"`
+	CreatedOn string `json:"created_on"`
+	ModifiedOn string `json:"modified_on"`
+	DeletedOn string `json:"deleted_on"`
+	IsDel string `json:"is_del"`
+}
+
+type Tag struct{
+	*Model
+	Name string `json:"name"`
+	State uint8 `json:"state"`
+}
+
+func (t Tag)TableName() string {
+	return "blog_tag"
+}
